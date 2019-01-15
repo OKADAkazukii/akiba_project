a9b53f47b2c7268d3d5bd4baff2ef7bf281cfda3
root <root@localhost.localdomain>
2019-01-15 13:44:33 +0900

    aaaaa

--- LaravelProject/resources/views/Admin/holiday.blade.php
+++ LaravelProject/resources/views/Admin/holiday.blade.php
@@ -1,111 +1,26 @@
+<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 @extends('layouts.app')
 @section('content')
-<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-<?php
-function h($s){
-  return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
-}
-
-try{
-  if(!isset($_GET{$t})|| !preg_match('/\A\d{4}-\d{2}\z/',$_GET{$t})){
-    throw new Exception();
-  }
-  $thisMonth = new DateTime($_GET{$t});
-} catch (Exception $e){
-  $thisMonth = new DateTime('first day of this month');
-}
-
-$dt = clone $thisMonth;
-$prev = $dt->modify('-1 month')->format('Y m');
-$dt = clone $thisMonth;
-$next = $dt->modify('+1 month')->format('Y m');
-
-$yearMonth = $thisMonth->format('F Y');
-
-
-
-$tail = '';
-$lastDayOfPrevMonth = new DateTime('last day of '.$yearMonth.'-1 month');
-while ($lastDayOfPrevMonth->format('w') < 6){
-  $tail =sprintf('<td class="gray">%d</td>',$lastDayOfPrevMonth->format('d')).$tail;
-  $lastDayOfPrevMonth->sub(new DateInterval('P1D'));
-}
-
-$body = '';
-$period = new DatePeriod(
-  new DateTime('first day of '.$yearMonth),
-  new DateInterval('P1D'),
-  new DateTime('first day of '.$yearMonth.'+1 month')
-);
-
-$holidays ='';
-$holi ='';
-//土曜日で折り返す処理,曜日に色を付ける処理
-foreach ($period as $day){
-  if($day->format('w') % 7=== 0){ $body .= '</tr><tr>';}
-  $body .= sprintf('<td class="youbi_%d">%d</td>',$day->format('w'),$day->format('d'));         
-}
-
-foreach ($period as $days){
-  $holidays = $days->format('Y-m-d');
-  foreach ($holiget as $holigets){
-      if($holigets->holiday == $holigets){
-      $holi = sprintf('<td class="holiday">%d</td>',$days->format('Y-m-d')); 
-    }
-  }
-}
-
-//来月1たちの曜日から月末の土曜まで表示
-$head = '';
-$firstDayOfNextMonth = new DateTime('first day of'.$yearMonth.'+1 month');
-while ($firstDayOfNextMonth->format('w')>0){
-  $head .= sprintf('<td class="gray">%d</td>',$firstDayOfNextMonth->format('d'));
-  $firstDayOfNextMonth->add(new DateInterval('P1D'));
-}
-?>
 <body>
-  <div>
+    <div class="insertholiday">{{ session('addholiday') }}</div>
     @if (session('message'))
       <div class="alert alert-success">{{ session('message') }}</div>
     @endif
   </div>
-  <br><br>
-    <form action="/holidayupdate" method="post">
-      {{ csrf_field() }}
-      <button style="position:relative; top:600px;" type="submit" class="btn btn-success">祝日更新</button>
-    </form>
+  <br>
+  <div>
+    <h3>休日設定</h3>
     <form action="/addholiday" method="post">
       {{ csrf_field() }}
       <label>日付 :<input type="date" name="date" required></label>
       <input type="submit" value="送信">
     </form>
-  <table>
-  <thead>
-    <tr> 
-      <th><a href="holiday/?t=<?php echo h($prev);?>">&laquo;</a></th>
-      <th colspan="5"><?php echo h($yearMonth); ?></th>
-      <th><a href="holiday/?t=<?php echo h($next);?>">&raquo;</a></th>
-    </tr>   
-  </thead>
-  <tbody>
-    <tr>
-    <td>Sun</td>
-    <td>Mon</td>
-    <td>The</td>
-    <td>Wed</td>
-    <td>Thu</td>
-    <td>Fri</td>
-    <td>Sat</td>
-    </tr>
-    <tr>
-    <?php echo $tail .$body .$holi .$head ;?> 
-    </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <th colspan="7"><a href="/holiday">Today</a></th>
-    </tr>
-  </tfoot>       
-  </table>
+  </div>
+  <div>
+    <form action="/holidayupdate" method="post">
+      {{ csrf_field() }}
+      <button style="position:relative; top:225px;" type="submit" class="btn btn-success">祝日更新</button>
+    </form>
+  </div>
 </body>
 @endsection
--- LaravelProject/resources/views/employee/addemployee.blade.php
+++ LaravelProject/resources/views/employee/addemployee.blade.php
@@ -3,6 +3,7 @@
 <script type="text/javascript" src="/js/style.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js"></script>
 
 @extends('layouts.app')
 @section('content')
@@ -32,47 +33,64 @@
             ?>
             <input type="hidden" value="{{$login_hash}}" name="login_hash"/>
             <div>
-                <div class="form_text">基本給</div>
-                <input type="text" name="basic_salary" onblur="test(this)" size="19" maxlength="7" style="text-align: right" id="s_form" value=""/>円
+                <div class="form_text" >基本給</div>
+                <input type="text" name="basic_salary" onblur="test(this)" size="19" maxlength="7" style="text-align: right" id="s_form" value=""/>
                 <div>※半角数値以外を入力するとアラートが表示されます。</div>
+                <div>※基本給または時給を入力し基本勤務時間を選択すると、もう一方の値が自動計算されます。</div> 
+            </div> 
+            <div style="position:relative; left:210px;bottom:120px; ">        
+            <div class="form_text">時給</div>
+                <input type="text" name="time_salary" onblur="test(this)" size="19" maxlength="7" style="text-align: right" id="t_form" value=""/>
             </div>
-
-            <div>
-                <div class="form_text">時給</div>
-                    <input type="text" name="time_salary" onblur="test(this)" size="19" maxlength="7" style="text-align: right" id="t_form" value=""/>円
-                </div> 
-            <br>
-                <select name="basic_or_time" style="width:30%;" id="changeSelect"  onchange="salarychange();">
+           
+                <select name="basic_or_time" style="width:30%;position:relative; bottom:35px;" id="changeSelect"  onchange="salarychange();">
                     <option value="" selected disabled hidden>--計算給与の選択--</option>
                     <option value="0">基本給</option>
                     <option value="1">時給（アルバイト等）</option>
                 </select>
 
             <div class="form_text">基本勤務時間(日)</div>
-                <input class="form" type="time" name="basic_work_time"　style="width:100px;font-size:18px;text-align:center;"/><br>
+                <input class="form" type="time" name="basic_work_time" id="basic_work_time"　style="width:100px;font-size:18px;text-align:center;" value="00:00"/><br>
             <br>
                 <input type="submit" value="作成" class="button1">
-         </div>
+        </div>
      </form>
 </div>
 <script>
+
 $(function() {
-  var $input = $('#s_form');
-  $input.on('change', function(event) {
-    var value = eval($input.val()) / eval(20.33*8);
-    var result= Math.ceil(value);
+  var $input = $('#s_form'); 
+  var basic_work_time = $('#basic_work_time');
+   $input.on('input', function(event){
+     document.getElementById('changeSelect').value = ('0');
+   basic_work_time.on('input', function(event){
+     var basic = $('#basic_work_time').val();
+     var numberHh = Number(moment(basic, 'HH:mm').format('H'));
+     var numberHm = Number(moment(basic, 'HH:mm').format('m'));
+     var time = eval(numberHh * 60 + numberHm) / eval(60);
+     var times= time.toFixed(2);
+     var val = eval($input.val()) / eval(20.33 * times );
+     var result= Math.ceil(val);
     document.getElementById('t_form').value = (result);
-    document.getElementById('t_form').readOnly = true;
+    });
   });
 });
 
 $(function(){
   var $tms = $('#t_form');
+  var basic_work_time = $('#basic_work_time');
   $tms.on('change', function(event){
-    var value = eval($tms.val()) * eval(20.33*8);
+    document.getElementById('changeSelect').value = ('1');
+  basic_work_time.on('change', function(event){
+    var time = $('#basic_work_time').val();
+    var numberHh = Number(moment(time, 'HH:mm').format('H'));
+    var numberHm = Number(moment(time, 'HH:mm').format('m'));
+    var basic = eval(numberHh * 60 + numberHm) / eval(60);
+    var basics= basic.toFixed(2);
+    var value = eval($tms.val()) * eval(20.33* basics);
     var result = Math.ceil(value);
     document.getElementById('s_form').value =(result);
-    document.getElementById('s_form').readOnly = true;
+    });
   });
 });
 
